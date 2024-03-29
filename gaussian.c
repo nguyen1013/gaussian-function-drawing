5d11dfde94e2e6246dfce521124f21a419fe5b3a
Nguyen Nguyen <nasongnguyen@gmail.com>
2024-03-29 09:38:50 +0200

    Change the character from "|" to block char for a more visualization chart.

--- gaussian.c
+++ gaussian.c
@@ -3,6 +3,7 @@
 #include "screen.h"
 
 void gaussianDrawing(int a, int c) {
+  unsigned char pipe = 221; // char code of a block pipe
   int x, y; // coordinate variables (x=0 at column 40, and y=1 at the row 25)
   clearScreen();
   gotoXY(1, 30);
@@ -14,12 +15,13 @@ void gaussianDrawing(int a, int c) {
   resetColors();
 
   // start drawing Gaussian  
+  setFGcolor(CYAN);
   for (y = a; y > 0; y--) {
     x = ceil(c * sqrt(2 * log((double)a / y))); // solving x according to y from the Gaussian function y = a*e^(-x^2/(2*c^2)
-    setFGcolor(RED + y % 7);    
+    //setFGcolor(RED + y % 7);    
     gotoXY(26-y, 40 - x); // drawing from the top of the chart, with column from 40-x to the column 40+x;
     for (int i = 0; i < 2 * x + 1; i+=1) { 
-      printf("|");
+      printf("%c", pipe);
     }
   }
   resetColors();
